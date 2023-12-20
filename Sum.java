aa6fd314c3f8c0b79c2a3b887f35c76142f75ae6
Demo <pratyushprasad.cse2022@nsec.ac.in>
2023-12-20 22:23:44 +0530

    done now please show1

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int summation=a+b;
+        System.out.println("The sum is"+summation);
+    }
+}
